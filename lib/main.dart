47f943c0d64ab0ecc8d46e636dbdadd554bc8f36
qpthedev <qpthedev@gmail.com>
2023-04-15 21:48:08 -0400

    HttpOverrides for image.network display issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webtoon/screens/home_screen.dart';
 
+class WebtoonHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  HttpOverrides.global = WebtoonHttpOverrides();
   runApp(const MyApp());
 }
 
